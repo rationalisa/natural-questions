630ea98001de024c1cd463690beceb525c307640
rationalisa <kez040@ucsd.edu>
2021-04-23 01:25:08 -0700

    modify run_exp

--- .ipynb_checkpoints/run_exp-checkpoint.sh
+++ .ipynb_checkpoints/run_exp-checkpoint.sh
@@ -1,7 +1,7 @@
-data_dir = "/storage/BERT_SQUAD_TOK" # dir to save the preprocessed json file
-save_dir = "/storage/model/BERT_SQUAD_TOK_L3" # dir to save the models
-val_path = "/storage/datset/v1.0-simplified_nq-dev-all.jsonl.gz"
-cache_dir = "/storage/datset" 
+data_dir="/storage/BERT_SQUAD_TOK" # dir to save the preprocessed json file
+save_dir="/storage/model/BERT_SQUAD_TOK_L3" # dir to save the models
+val_path="/storage/datset/v1.0-simplified_nq-dev-all.jsonl.gz"
+cache_dir="/storage/datset" 
 
 # Prepare training examples
 python3 preprocess.py --pre_size 0 --total_size 300000 --train --data_dir ${data_dir} --cache_dir ${cache_dir}
@@ -13,11 +13,10 @@ python3 preprocess.py --pre_size 300000 --total_size 300050 --sample_size 50 --d
 python3 train_squad.py --cp checkpoint-1200 --save_dir ${save_dir} --data_dir ${data_dir}
 
 # Evaluate using dev-all dataset
-python3 evaluate.py --cp checkpoint-1200 --dir_name ${save_dir} --diff_threshold -5 --val_path ${val_path}
-#python3 evaluate.py --cp checkpoint-6500 --dir_name BERT_SQUAD_TOK_L2 --diff_threshold -5
+python3 evaluate.py --cp checkpoint-1200 --save_dir ${save_dir} --diff_threshold -5 --val_path ${val_path}
 
 # Compute the F1 score
-pred_path = "$save_dir/eval_predictions.json"
+pred_path="$save_dir/eval_predictions.json"
 ## ensemble ## 
 # pred_path = "$save_dir/eval_predictions1.json@$save_dir/eval_predictions2.json"
 python3 nq/nq_eval.py --gold_path=${val_path} --predictions_path=${pred_path} --pretty_print
--- nq/run_eval.sh
+++ nq/run_eval.sh
@@ -1,4 +1,4 @@
 #python3 nq_eval.py --gold_path=/storage/datset/v1.0_sample_nq-dev-sample.jsonl.gz --predictions_path=/storage/model/BERT_SQUAD_TOK_L3/cp1200_pred_7.json --pretty_print
-python3 nq_eval.py --gold_path=/storage/datset/v1.0-simplified_nq-dev-all.jsonl.gz --predictions_path=/storage/model/BERT_SQUAD_TOK_L2/cp6500_pred_L_5.json --pretty_print
-#/storage/model/BERT_SQUAD_TOK_L3/cp1200_pred_L.json@
+python3 nq_eval.py --gold_path=/storage/datset/v1.0-simplified_nq-dev-all.jsonl.gz --predictions_path=/storage/model/BERT_SQUAD_TOK_L3/cp1200_pred_L_5.json@/storage/model/BERT_SQUAD_TOK_L2/cp6500_pred_L.json --pretty_print
+
 
